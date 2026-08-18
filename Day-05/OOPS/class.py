1520481f0c11aadfb17fd338152995f5f4ec8b94
Sajal vishwakarma <sajalvis8627@gmail.com>
2026-08-18 08:07:24 +0530

    Add Day-05 OOPS

--- Day-05/OOPS/class.py
+++ Day-05/OOPS/class.py
@@ -8,4 +8,4 @@ class Student:
 stu1 = Student()
 
 print(f"{stu1.name} is the name of the student.") #accessing the name attribute of the Student class
-print(f"{stu1.subject} is the subject of the student.") #accessing the subject attribute of the Student class
+print(f"{stu1.subject} is the subject of the student {stu1.name}.") #accessing the subject attribute of the Student class
