819783807a2f953e52fb5fc60977286446252fea
Sajal vishwakarma <sajalvis8627@gmail.com>
2026-08-19 10:03:59 +0530

    Add Day-05 OOPS

---
+++ Day-05/OOPS/Four pillars/Polymorphisms/polymorphisms.py
@@ -0,0 +1,17 @@
+class Dog:
+    def sound(self):
+        print("Dog barks")
+
+
+class Cat:
+    def sound(self):
+        print("Cat meows")
+
+
+dog = Dog()
+cat = Cat()
+
+dog.sound()
+cat.sound()
+
+#Here, the method name sound() is the same, but its behavior is different.
---
+++ Day-05/OOPS/Four pillars/Polymorphisms/types of polymorphisms/OperatorPolymorphism.py
@@ -0,0 +1,5 @@
+print(10 + 20)
+print("Hello " + "World")
+
+print(5 * 3)
+print("Hi " * 3)
